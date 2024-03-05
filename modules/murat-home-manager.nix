70f6d8da63897660c2e28fe98389c5665ffca1b0
Murat Cabuk <muratcabuk@live.com>
2024-03-05 22:12:21 +0300

    modified:   modules/murat-home-manager.nix

--- modules/murat-home-manager.nix
+++ modules/murat-home-manager.nix
@@ -47,6 +47,12 @@
                                     src = pkgs.zsh-powerlevel10k;
                                     file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
                                   }
+
+                                  {
+                                    name = "powerlevel10k-config";
+                                    src = ./p10k-config;
+                                    file = "p10k.zsh";
+                                  }
                                 ];
                       oh-my-zsh = {
                                     enable = true;
