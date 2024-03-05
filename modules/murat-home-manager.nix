9bc499c40da780bbac101862ef6d9bea7d912a61
Murat Cabuk <muratcabuk@live.com>
2024-03-05 07:54:07 +0300

    modified:   modules/murat-home-manager.nix

--- modules/murat-home-manager.nix
+++ modules/murat-home-manager.nix
@@ -7,23 +7,9 @@
 
   
 
-  # https://www.reddit.com/r/NixOS/comments/12v2flc/struggling_with_very_basic_homemanager_config/
-  #home.sessionVariables = { LANG = "en_US.UTF-8"; };
-  # Language ayaraları
-   home.language = {
-     base = "en_US.utf8";
-     ctype = "en_US.utf8";
-     numeric = "tr_TR.utf8";
-     time = "tr_TR.utf8";
-     collate = "tr_TR.utf8";
-     monetary = "tr_TR.utf8"; #para
-     messages = "en_US.utf8";
-     paper = "tr_TR.utf8";
-     name = "tr_TR.utf8";
-     address = "tr_TR.utf8";
-     telephone = "tr_TR.utf8";
-     measurement = "tr_TR.utf8";
-};
+
+  # Language ayaraları : https://nix-community.github.io/home-manager/options.xhtml#opt-home.language
+   home.language.base = en_US.UTF-8;
 
 
   # bu değeri de ilk kuruulumdaki gibi bırakıyoruz. 
