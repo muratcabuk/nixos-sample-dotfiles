ae7ce9c50e6e386a4144872a1ea6548a4fa98a01
Murat Cabuk <muratcabuk@live.com>
2024-03-06 21:50:34 +0300

    modified:   modules/murat-home-manager.nix

--- modules/murat-home-manager.nix
+++ modules/murat-home-manager.nix
@@ -36,6 +36,11 @@
 
     programs.zsh = {
 
+                      initExtraBeforeCompInit = ''
+                              # p10k instant prompt
+                              P10K_INSTANT_PROMPT="$XDG_CACHE_HOME/p10k-instant-prompt-''${(%):-%n}.zsh"
+                              [[ ! -r "$P10K_INSTANT_PROMPT" ]] || source "$P10K_INSTANT_PROMPT"
+                      '';
 
                       history.size = 10000;
                       enable = true;
